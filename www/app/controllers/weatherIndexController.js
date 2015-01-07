3e7b5880980bb9517e3add5860b502b1147c3e2f
Vinicius Rafael Tavares <vinicius.rafael19@hotmail.com>
2015-01-07 18:45:42 -0200

    Consumindo API do Yahoo Weather

--- www/app/controllers/weatherIndexController.js
+++ www/app/controllers/weatherIndexController.js
@@ -1,9 +1,21 @@
 (function () {
-    var controllerId = 'weatherIndexController';
+	var controllerId = 'weatherIndexController';
 
-    angular.module('app').controller(controllerId, weatherIndexController) 
+	angular.module('app').controller(controllerId, ['$scope','weatherService', weatherIndexController]);
 
-        function weatherIndexController() { 
-            var vm = this;
-        } 
+	function weatherIndexController($scope, weatherService) { 
+		var vm = this;
+
+		weatherService.getCurrentWeather().then(function(response){ 
+
+			$scope.forecasts = response.data.query.results.channel.item.forecast;  
+
+			$scope.currentForecast = $scope.forecasts[0];
+
+			$scope.currentForecast.high = Math.round(($scope.currentForecast.high - 32) / 1.8); 
+
+			$scope.currentForecast.low = Math.round(($scope.currentForecast.low - 32) / 1.8); 
+
+		})
+	} 
 })();
--- www/app/services/weatherService.js
+++ www/app/services/weatherService.js
@@ -1,9 +1,29 @@
 var app = angular.module('app');  
 
-var service = 'weatherService';  
+var serviceId = 'weatherService';  
 
-app.factory(service, ['$http', weatherService]);
+app.factory(serviceId, ['$http', '$q', weatherService]);
 
-var weatherService = function() { 
-	return 'dee';  
+
+function weatherService($http, $q) { 
+	return { 
+		getCurrentWeather : getCurrentWeather 
+	}
+
+	function getCurrentWeather() {
+
+		var url = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22s%C3%A3o%20paulo%2C%20sp%22)&format=json';
+
+		var deferred = $q.defer();
+		return $http.get(url)
+
+		.success(function(response) {
+			deferred.resolve(response);
+		})
+		.error(function(response) {
+			deferred.reject(response);
+		});
+	}
+
+
 }
